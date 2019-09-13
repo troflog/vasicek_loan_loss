5dcd43c41ba194c657d2fbe7f601267d2e5f56ff
trond <troflog@gmail.com>
2019-09-13 19:30:22 +0200

    Simple plotting

--- merton.R
+++ merton.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 pd = 0.25
-rho = 0.8
-x <- seq(0,1,by=0.0001)
+rho = 0.9999
+x <- seq(0,1,by=0.001)
 x <- x[2:(length(x)-1)]
 x[1]<-0.00001
 x[length(x)]<-(1-x[1])
@@ -11,9 +11,25 @@ x_norm <- x/n
 c <- pnorm((sqrt(1-rho)* qnorm(x)-qnorm(pd))/sqrt(rho))
 #Densisty of loan loss distribution
 t <- sqrt((1-rho)/rho)*exp( -1/(2*rho)*(sqrt(1-rho)*qnorm(x)-qnorm(pd))^2+0.5*qnorm(x)^2)
+print(sqrt((1-rho)/rho)*exp( -1/(2*rho)*(sqrt(1-rho)*qnorm(x[1])-qnorm(pd))^2+0.5*qnorm(x[1])^2))
+vfun <- function(x){
+  pd = 0.25
+  rho = 0.8
+  t <- sqrt((1-rho)/rho)*exp( -1/(2*rho)*(sqrt(1-rho)*qnorm(x)-qnorm(pd))^2+0.5*qnorm(x)^2)
+  return(t)
+}
+area <- integrate(vfun,x[1],x[length(x)])$value
+
 
 #Putting results into a dataframe
 vas <- data.frame(x,x_norm,c,t)
+
+#Plot cummulative distribution
+plot(x,c,type = 'l',ylim = c(0,1))
+lines(x,p)
+plot(x,t,type = 'l')
+
+
 
 
 
